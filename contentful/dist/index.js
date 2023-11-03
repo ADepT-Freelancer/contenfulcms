d9067c6aa88c13d9b637da04dee337677f0d0f70
ADepTUAKh <fuchkoaa@gmail.com>
2023-11-03 11:18:41 +0100

    i trying

--- contentful/dist/index.js
+++ contentful/dist/index.js
@@ -1,13 +1,9 @@
 "use strict";
 exports.__esModule = true;
 exports.client = void 0;
-var contentful = require("contentful");
-exports.client = contentful.createClient({
+var contentful_1 = require("contentful");
+exports.client = contentful_1.createClient({
     space: "ofpvgsovd02l",
     environment: "master",
     accessToken: "IsjfY3wmrWp2oj2Df1ah0hYT160ZFqL3hzUWI5uufOs"
 });
-exports.client
-    .getEntry("2DBqg3CspNynqUmQc4E9V7")
-    .then(function (entry) { return console.log(entry); })["catch"](console.error);
-console.log("contentful api");
--- contentful/index.ts
+++ contentful/index.ts
@@ -5,13 +5,3 @@ export const client = createClient({
   environment: "master", // defaults to 'master' if not set
   accessToken: "IsjfY3wmrWp2oj2Df1ah0hYT160ZFqL3hzUWI5uufOs",
 });
-
-// export const client = contentful.createClient({
-
-// const contentful = require("contentful");
-// client
-//   .getEntry("2DBqg3CspNynqUmQc4E9V7")
-//   .then((entry: any) => console.log(entry))
-//   .catch(console.error);
-
-// console.log("contentful api");
